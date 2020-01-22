a0a206fa4e4f80a769cadde1bef0c962c1d78404
Max Maes <max@livewallgroup.com>
2020-01-22 11:38:20 +0100

    Fixed android setup

--- android/src/main/java/nl/livewall/reactnative/LivewallComponentsModule.java
+++ android/src/main/java/nl/livewall/reactnative/LivewallComponentsModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package nl.livewall.reactnative;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
