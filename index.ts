4c55059059ea4a34e9d53e1acd8d991c4ad89d73
Remy Baratte <remy@livewallgroup.com>
2020-01-24 08:35:00 +0100

    Added page control component.

--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
-import { NativeModules } from 'react-native';
-import { Card } from './src/components/card';
-import { Divider} from './src/components/divider';
-import { TextInput } from './src/components/textinput';
-import { Touchable } from './src/components/touchable';
+import { NativeModules } from "react-native";
+import { Card } from "./src/components/card";
+import { Divider } from "./src/components/divider";
+import { TextInput } from "./src/components/textinput";
+import { Touchable } from "./src/components/touchable";
+import { PageControl } from "./src/components/page-control";
 
 const { LivewallComponents } = NativeModules;
 
-export {
-    Card,
-    Divider,
-    LivewallComponents,
-    TextInput,
-    Touchable
-};
+export { Card, Divider, LivewallComponents, PageControl, TextInput, Touchable };
